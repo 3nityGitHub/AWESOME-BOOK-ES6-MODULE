f2c81c6596e55dc688e202db93ff99f1b0230813
3nityGitHub <60592651+3nityGitHub@users.noreply.github.com>
2023-05-08 23:54:23 +0100

    "Import modules into index.js files"

---
+++ Modules/timeSetup.js
@@ -0,0 +1,14 @@
+import { DateTime } from './luxon.js';
+
+export const getTime = () => {
+  const date = document.querySelector('#date');
+  if (!date.innerText) {
+    setInterval(() => {
+      date.innerText = DateTime.now().toLocaleString(
+        DateTime.DATETIME_MED_WITH_SECONDS
+      );
+    }, 1000);
+  }
+};
+
+export default getTime;
---
+++ index.js
@@ -0,0 +1,11 @@
+
+import { bookNav } from "./Modules/bookNav.js";
+import { bookSet } from './Modules/bookSet.js';
+import {getTime} from "./Modules/timeSetup.js";
+
+document.addEventListener('click', () => {
+  bookNav();
+  bookSet()
+  getTime()
+
+});
